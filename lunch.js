c5a8acbc431468c99dd993b94636cbfe2b327f61
cash <cash@cashwu.com>
2017-08-25 10:44:27 +0800

    modify

--- lunch.js
+++ lunch.js
@@ -1,14 +1,11 @@
-$("<button id='comment' class='btn btn-primary' style='margin-bottom: 10px;' data-toggle='modal' data-target='#lunchModal'>Comment</button>").insertAfter("h2");
+var btn = `<button id='comment' class='btn btn-primary' style='margin-bottom: 10px;' data-toggle="modal" data-target="#lunchModal" >Comment</button>`;
 
 var modal =
     `<div class="modal" id="lunchModal" >
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Comment</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    <h5 class="modal-title" style="font-weight:bold;">Comment</h5>
                 </div>
                 <div class="modal-body" id="body">
                 </div>
@@ -16,36 +13,64 @@ var modal =
                     <div class="form-group mx-sm-3">
                         <input class="form-control" id="cmt"></textarea>
                     </div>
-                    <button id="AddCmt" type="button" class="btn btn-secondary">Save</button>
+                    <button id="AddCmt" type="button" class="btn btn-primary">Save</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
     </div>
     `;
 
-$(modal).insertAfter("#submitForm");
+if (location.pathname.indexOf("ClientOrder") > 0) {
 
-$("#AddCmt").click(function() {
-    var cmt = $("#cmt").val();
-    var p = '<p>' + $("#cmt").val() + '</p>';
-    $(p).appendTo("#body");
+    $(btn).insertAfter("h2");
 
-    $("#cmt").val("");
-    PostData(cmt);
-});
+    $(modal).insertAfter("#submitForm");
 
-GetData();
+    $("#AddCmt").click(function(e) {
+        e.preventDefault();
+
+        var cmt = $("#cmt").val().trim();
+        if (cmt == '') {
+            alert("comment can't be empty !!");
+            return;
+        }
+
+        var p = '<p>' + cmt + '</p>';
+        $(p).appendTo("#body");
+
+        EmptyCmt();
+        PostData(cmt);
+    });
+
+    $("#comment").click(function(e) {
+        e.preventDefault();
+        EmptyCmt();
+        GetData();
+    });
+}
 
 console.log("load done..");
+
+function EmptyCmt() {
+    $("#cmt").val("");
+}
 
 function GetData() {
     $.ajax({
         type: "Get",
         url: "http://ti03pc03:8899/?name=" + $("h2").text(),
         dataType: "json",
-        success: function(response) {
-            console.log(response);
-        }
+        success: GetDataSuccess
+    });
+}
+
+function GetDataSuccess(response) {
+    $("#body").empty();
+
+    $.each(response, function(index, value) {
+        var p = '<p>' + value.Comment1 + '</p>';
+        $(p).appendTo("#body");
     });
 }
 
@@ -60,6 +85,9 @@ function PostData(cmt) {
         dataType: "json",
         success: function(response) {
             console.log(response);
+            var msg = "Save " + (response.isSuccess ? "Success" : "failed");
+            alert(msg);
         }
+
     });
 }
